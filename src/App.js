fc5083d884e22113a931c4deca121853ca40f928
fiopwk <flopwiki@gmail.com>
2020-03-02 15:07:30 -0500

    fixed connect and state to props

--- src/App.js
+++ src/App.js
@@ -7,36 +7,42 @@ import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-import { buyItem, removeFeature} from '../src/actions/index'
-import { reducer, initialState} from '../src/reducers/index';
+import { buyItem, removeFeature } from '../src/actions/index'
+import { reducer, initialState } from '../src/reducers/index';
 
 
-const App = () => {
+const App = (state) => {
 
   return (
     <div className="boxes">
       <div className="box">
         <Header car={state.car} />
-        <AddedFeatures 
-        car={state.car} 
-        removeFeature={removeFeature}
+        <AddedFeatures
+          car={state.car}
+          removeFeature={removeFeature}
         />
       </div>
       <div className="box">
-        <AdditionalFeatures 
-        additionalFeatures={state.additionalFeatures}
-        buyItem={buyItem}
+        <AdditionalFeatures
+          additionalFeatures={state.additionalFeatures}
+          buyItem={buyItem}
         />
         <Total car={state.car} additionalPrice={state.additionalPrice} />
       </div>
     </div>
   );
-};  
+};
 
-const mapStateToProps = state => {
+const mapStateToProps = {
   buyItem,
-  removeFeature
+    removeFeature
 }
 
 
-export default connect(mapStateToProps) (App);
+export default connect(
+  state => ({
+    car: state.car,
+    additionalFeatures: state.additionalFeatures,
+    additionalPrice: state.additionalPrice
+  }),
+  mapStateToProps)(App);
