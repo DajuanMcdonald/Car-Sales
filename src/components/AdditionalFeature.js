3cc72c9b4fa2ca00e406cad4ecfde5cbf498166c
fiopwk <flopwiki@gmail.com>
2020-03-01 19:50:46 -0500

    break onClick but show log on props object

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -5,7 +5,7 @@ const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button onClick={() => buyFeature(feature)} className="button">Add</button>
+      <button onClick={() => console.log(props)} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
