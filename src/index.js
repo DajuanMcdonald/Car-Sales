a2fb822960b2f6b2d1fcb9a7134d30d5cf4f39e5
fiopwk <flopwiki@gmail.com>
2020-03-02 13:39:20 -0500

    moved state to reducers file in reducers/index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
-
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider ><App /></Provider>,rootElement);
---
+++ src/reducers/index.js
@@ -0,0 +1,42 @@
+const initialState = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+  };
+  
+  // set up reducer and initial state
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "BUY_ITEM":
+        return {
+          ...state,
+          additionalPrice: state.additionalPrice + action.item.price,
+          car: { ...state.car, features: [...state.car.features, action.item] },
+          additionalFeatures: state.additionalFeatures.filter(feat => feat.id !== action.item.id)
+        };
+  
+      case "REMOVE_ITEM":
+        return {
+          ...state,
+          additionalPrice: state.additionalPrice - action.item.price,
+          car: { ...state.car, features: state.car.features.filter((feat) => feat.id !== action.item.id) },
+          additionalFeatures: [...state.additionalFeatures, action.item]
+  
+        }
+  
+      default:
+  
+        return state;
+    }
+  }
