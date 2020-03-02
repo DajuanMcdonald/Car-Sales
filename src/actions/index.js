5ebedb0aa6a82656feecd8d688a0d1cf8043249c
fiopwk <flopwiki@gmail.com>
2020-03-02 15:17:12 -0500

    added action to onClick

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,12 @@ export const BUY_ITEM = 'BUY_ITEM';
 export const REMOVE_ITEM = 'REMOVE_ITEM';
 
 
-export const buyItem = item => {
+export const buyItem = (item) => {
   // dipsatch an action here to add an item
-  return { type: 'BUY_ITEM', item }
+  return { type: 'BUY_ITEM', payload: item }
 };
 
-export const removeFeature = item => {
+export const removeFeature = (item) => {
     // dispatch an action here to remove an item
-    return { type: 'REMOVE_ITEM', item }
+    return { type: 'REMOVE_ITEM', payload: item }
   };
